76519ea13093f7cc7424122b49aa846bf4521bdb
Federico Nafria <federiconafria@gmail.com>
2018-12-26 13:16:34 +0100

    Separating environments in multiple files
    - Now each environment is defined in a separate file, e.g. java.env to define java environment
    - Added validations for parameters passed
    - Added validations for variables sourced from env file (image and aliases)

--- bootstrap_environment.sh
+++ bootstrap_environment.sh
@@ -1,14 +1,28 @@
-source recipe.env
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	>&2 echo "USAGE: $(basename "$0") recipe";
+	exit 1;
+fi;
+recipefile=$1.env
+
+if [ ! -f "$recipefile" ]; then
+	>&2 echo "$recipefile does not exist";
+	exit 1;
+fi;
+source "$recipefile";
+image=${image?"No image defined in recipe"};
+aliases=${aliases?"No aliases defined in recipe"};
 
 container_name=$image"_env"
 
-docker run -d --name $container_name $image tail -f /dev/null
+docker run -d --name "$container_name" "$image" tail -f /dev/null
 
 base_command="docker exec -i $container_name"
 
-aliases_list=$(echo $aliases | tr "," "\n");
+aliases_list=$(echo "$aliases" | tr "," "\n");
 
 for alias in $aliases_list
 do
-    alias $alias="$base_command $alias";
+    alias "$alias=$base_command $alias";
 done
